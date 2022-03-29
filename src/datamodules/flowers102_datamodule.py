476b46c9228eeb852f83c95cbe2dadb99820e9c8
iwawiwi <iwawiwi@gmail.com>
2022-03-29 21:17:30 +0800

    implement alexnet

---
+++ src/datamodules/flowers102_datamodule.py
@@ -0,0 +1,110 @@
+from typing import Optional, Tuple
+
+import torch
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import Flowers102
+from torchvision.transforms import transforms
+
+
+class Flowers102DataModule(LightningDataModule):
+    """Example of LightningDataModule for Flowers102 dataset.
+
+    A DataModule implements 5 key methods:
+        - prepare_data (things to do on 1 GPU/TPU, not on every GPU/TPU in distributed mode)
+        - setup (things to do on every accelerator in distributed mode)
+        - train_dataloader (the training dataloader)
+        - val_dataloader (the validation dataloader(s))
+        - test_dataloader (the test dataloader(s))
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/extensions/datamodules.html
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        batch_size: int = 64,
+        num_workers: int = 4,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        self.save_hyperparameters(logger=False)
+
+        # compose toTensor() and Normalize(mean, std)
+        self.transform_fn = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((227, 227)),  # resize to 227x227 for AlexNet
+            ]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    @property
+    def num_classes(self) -> int:
+        return 102
+
+    def prepare_data(self):
+        """Download data if needed.
+
+        This method is called only from a single GPU.
+        Do not use it to assign state (self.x = y).
+        """
+        Flowers102(self.hparams.data_dir, split="train", download=True)
+        Flowers102(self.hparams.data_dir, split="val", download=True)
+        Flowers102(self.hparams.data_dir, split="test", download=True)
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by lightning when doing `trainer.fit()` and `trainer.test()`,
+        so be careful not to execute the random split twice! The `stage` can be used to
+        differentiate whether it's called before trainer.fit()` or `trainer.test()`.
+        """
+
+        # load datasets only if they're not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            self.data_train = Flowers102(
+                self.hparams.data_dir, split="train", transform=self.transform_fn
+            )
+            self.data_test = Flowers102(
+                self.hparams.data_dir, split="test", transform=self.transform_fn
+            )
+            self.data_val = Flowers102(
+                self.hparams.data_dir, split="val", transform=self.transform_fn
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
---
+++ src/models/components/alex_net.py
@@ -0,0 +1,68 @@
+from torch import nn
+
+
+class AlexNet(nn.Module):
+    """AlexNet
+    based on: https://blog.paperspace.com/popular-deep-learning-architectures-alexnet-vgg-googlenet/
+    also: https://github.com/dansuh17/alexnet-pytorch/blob/master/model.py
+    """
+
+    def __init__(
+        self,
+        in_channel: int = 3,
+        num_classes: int = 1000,
+    ):
+        super().__init__()
+
+        self.in_size = 227  # input size is 227x227
+        self.in_channel = in_channel
+        self.num_classes = num_classes
+
+        # -- convolution layers
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(in_channel, 96, kernel_size=11, stride=4),  # 227x227x3 -> 55x55x96
+            nn.ReLU(),
+            # normalize across local region
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # 55x55x96 -> 27x27x96
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),  # 27x27x96 -> 27x27x256
+            nn.ReLU(),
+            # normalize across local region
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # 27x27x256 -> 13x13x256
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),  # 13x13x256 -> 13x13x384
+            nn.ReLU(),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),  # 13x13x384 -> 13x13x384
+            nn.ReLU(),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),  # 13x13x384 -> 13x13x256
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # 13x13x256 -> 6x6x256
+        )
+        # -- fully connected layers
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(),  # for non-linearity
+            nn.Dropout(p=0.5),  # use dropout to avoid overfitting
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, num_classes),
+        )
+        self.__init_bias()  # initialize bias
+
+    def forward(self, x):
+        assert x.shape[2] == self.in_size, "Input must be 227x227"
+
+        x = self.conv_layer(x)
+        x = x.view(x.size(0), -1)  # flatten dimension, x.size(0) is batch size, -1 is inferred
+        x = self.fc_layer(x)
+        return x
+
+    def __init_bias(self):
+        for layer in self.conv_layer:
+            if isinstance(layer, nn.Conv2d):
+                nn.init.normal_(layer.weight, mean=0, std=0.01)
+                nn.init.constant_(layer.bias, 0)
+        nn.init.constant_(self.conv_layer[4].bias, 1)
+        nn.init.constant_(self.conv_layer[10].bias, 1)
+        nn.init.constant_(self.conv_layer[12].bias, 1)
